0c15579ab368ff2deba0612f29de3bfbc293c4c5
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2026-01-10 17:56:24 +0300

    Remove deprecated theme feature

--- src/main/java/ru/oldzoomer/Application.java
+++ src/main/java/ru/oldzoomer/Application.java
@@ -1,12 +1,11 @@
 package ru.oldzoomer;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+
 @SpringBootApplication
-@Theme("default")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
